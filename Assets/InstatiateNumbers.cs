c1b9264419bc1d919a7547c4fef3c9984c5ac550
EttyBlack <6166887@gmail.com>
2021-12-29 11:51:16 +0200

    Add Project

---
+++ Assets/InstatiateNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstatiateNumbers : MonoBehaviour
+{
+    [SerializeField] private Image[] numbersSprite;
+    [SerializeField] private Image[] transparencySprite;
+    [SerializeField] private Image transparency;
+    [SerializeField] private int numbersPlace = 0;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+
+            transparencySprite[numbersPlace].sprite = numbersSprite[numbersPlace].sprite;
+            numbersPlace++;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            for (int i = 0; i < transparencySprite.Length; i++)
+            {
+                transparencySprite[i].sprite = transparency.sprite;
+                numbersPlace = 0;
+                Debug.Log("Start Again");
+            }
+                
+        }
+    }
+}
