987a77ceec2cc0aff11e9ef9e45607b36e347932
ESTY-PORGES <ep0583217291@gmail.com>
2021-12-30 01:17:28 +0200

    mechanics

--- Assets/InstatiateNumbers.cs
+++ Assets/InstatiateNumbers.cs
@@ -10,17 +10,12 @@ public class InstatiateNumbers : MonoBehaviour
     [SerializeField] private Image transparency;
     [SerializeField] private int numbersPlace = 0;
 
+    private int myname;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
-        {
-
-            transparencySprite[numbersPlace].sprite = numbersSprite[numbersPlace].sprite;
-            numbersPlace++;
-        }
-
-        if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.D) 
+            || Input.GetKeyDown(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
             for (int i = 0; i < transparencySprite.Length; i++)
             {
@@ -28,7 +23,52 @@ public class InstatiateNumbers : MonoBehaviour
                 numbersPlace = 0;
                 Debug.Log("Start Again");
             }
-                
+
         }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            transparencySprite[numbersPlace].sprite = numbersSprite[numbersPlace].sprite;
+            numbersPlace++;
+        }
+
     }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        myname = 1;
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.S))
+    //    {
+    //        myname = 2;
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.S))
+
+
+
+
+    //if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+    //{
+
+    //    //transparencySprite[numbersPlace].sprite = numbersSprite[numbersPlace].sprite;
+    //    //numbersPlace++;
+    //}
+
+    //if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.W) && Input.GetKey(KeyCode.S))
+    //{
+    //    //for (int i = 0; i < transparencySprite.Length; i++)
+    //    //{
+    //    //    transparencySprite[i].sprite = transparency.sprite;
+    //    //    numbersPlace = 0;
+    //    //    Debug.Log("Start Again");
+    //    //}
+
+    //        //}
+    //}
+
+
+
 }
