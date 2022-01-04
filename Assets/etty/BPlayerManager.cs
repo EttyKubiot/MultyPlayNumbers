049b60036e0c9b6c158c5a0808f4840b024933d5
EttyBlack <6166887@gmail.com>
2022-01-04 22:15:10 +0200

    Add Menu Scene And PlayerPrefs

--- Assets/etty/BPlayerManager.cs
+++ Assets/etty/BPlayerManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class BPlayerManager : MonoBehaviour
 {
     [SerializeField] private BGameManager bGameManager;
+    [SerializeField] private MenuUi menuUi;
 
     public List<BPlayerData> players = new List<BPlayerData>();
     private List<KeyCode> myKeys = new List<KeyCode>();
@@ -14,8 +15,8 @@ public class BPlayerManager : MonoBehaviour
     //private List<InputField> inputName = new List<InputField>();
 
     public bool Pressed = false;
-    [SerializeField] private InputField inputField;
-    [SerializeField] private InputField inputField1;
+    //[SerializeField] private InputField inputField;
+    //[SerializeField] private InputField inputField1;
 
     public string myName;
     public string myName1;
@@ -28,7 +29,9 @@ public class BPlayerManager : MonoBehaviour
 
     void Awake()
     {
-        bGameManager.OnValueChanged += CheckIfOnValueChanged;
+        myName = PlayerPrefs.GetString("name2");
+        myName1 = PlayerPrefs.GetString("name3");
+        //bGameManager.OnValueChanged += CheckIfOnValueChanged;
         players.Add(new BPlayerData("", KeyCode.Alpha0));
         players.Add(new BPlayerData("", KeyCode.Alpha1));
         players[0].playerScore = 0;
@@ -36,6 +39,8 @@ public class BPlayerManager : MonoBehaviour
         for (int i = 0; i < players.Count; i++)
         {
             myKeys.Add(players[i].keyCode);
+            players[0].name = myName;
+            players[1].name = myName1;
             //playerName.Add(players[i].name = inputName[i].text);
         }
     }
@@ -44,16 +49,15 @@ public class BPlayerManager : MonoBehaviour
     public void CheckIfOnValueChanged()
     {
 
-        myName = inputField.text;
-        myName1 = inputField1.text;
-        for (int i = 0; i < players.Count; i++)
-        {
-            players[0].name = myName;
-            players[1].name = myName1;
-            //playerName.Add(players[i].name = inputName[i].text);
-        }
-        
-
+        //myName = inputField.text;
+        //myName1 = inputField1.text;
+        //for (int i = 0; i < players.Count; i++)
+        //{
+        //    players[0].name = myName;
+        //    players[1].name = myName1;
+        //    //playerName.Add(players[i].name = inputName[i].text);
+        //}
+ 
     }
 
 
@@ -71,10 +75,10 @@ public class BPlayerManager : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            bGameManager.OnValueChanged?.Invoke();
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    bGameManager.OnValueChanged?.Invoke();
+        //}
     }
 
     private IEnumerator ClickOver()
